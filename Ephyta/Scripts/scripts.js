1f5262cdafd66bc522e80786dae9c0ce21e87d01
haianhnguyen12082001@gmail.com <haianhnguyen12082001@gmail.com>
2022-09-07 10:45:52 +0700

    fix-css-home

--- Ephyta/Scripts/scripts.js
+++ Ephyta/Scripts/scripts.js
@@ -591,3 +591,23 @@ function UpdateToCard(id, changeValue) {
         }
     });
 }
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 150px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        mybutton.style.display = "flex";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
